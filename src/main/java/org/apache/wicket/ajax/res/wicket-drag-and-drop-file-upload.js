4cf65e73dfc4f9e51dfea09807a8ad1abc1f8175
Andrew Kondratev <andruhon@gmail.com>
2018-01-11 16:17:07 +1300

    add copy-paste from wicket 8

--- src/main/java/org/apache/wicket/ajax/res/wicket-drag-and-drop-file-upload.js
+++ src/main/java/org/apache/wicket/ajax/res/wicket-drag-and-drop-file-upload.js
@@ -109,6 +109,186 @@
         dropArea.addEventListener("dragenter", dragEnterHandler);
         dropArea.addEventListener("dragleave", dragEndHandler);
         dropArea.addEventListener("dragend", dragEndHandler);
+
+        // This block contains copy-paste from Wicket 8 wicket-ajax-jquery.js
+        {
+            var createIFrame,
+                getAjaxBaseUrl,
+                isUndef,
+                replaceAll,
+                htmlToDomDocument,
+                nodeListToArray;
+
+            isUndef = function (target) {
+                return (typeof(target) === 'undefined' || target === null);
+            };
+
+            replaceAll = function (str, from, to) {
+                var regex = new RegExp(from.replace( /\W/g ,'\\$&' ), 'g');
+                return str.replace(regex,to);
+            };
+
+            /**
+             * Creates an iframe that can be used to load data asynchronously or as a
+             * target for Ajax form submit.
+             *
+             * @param iframeName {String} the value of the iframe's name attribute
+             */
+            createIFrame = function (iframeName) {
+                // WICKET-6340 properly close tag for XHTML markup
+                var $iframe = jQuery('<iframe name="'+iframeName+'" id="'+iframeName+
+                    '" src="about:blank" style="position: absolute; top: -9999px; left: -9999px;"></iframe>');
+                return $iframe[0];
+            };
+
+            /**
+             * A safe getter for Wicket's Ajax base URL.
+             * If the value is not defined or is empty string then
+             * return '.' (current folder) as base URL.
+             * Used for request header and parameter
+             */
+            getAjaxBaseUrl = function () {
+                var baseUrl = Wicket.Ajax.baseUrl || '.';
+                return baseUrl;
+            };
+
+            /**
+             * Helper method that serializes HtmlDocument to string and then
+             * creates a DOMDocument by parsing this string.
+             * It is used as a workaround for the problem described at https://issues.apache.org/jira/browse/WICKET-4332
+             * @param htmlDocument (DispHtmlDocument) the document object created by IE from the XML response in the iframe
+             */
+            htmlToDomDocument = function (htmlDocument) {
+                var xmlAsString = htmlDocument.body.outerText;
+                xmlAsString = xmlAsString.replace(/^\s+|\s+$/g, ''); // trim
+                xmlAsString = xmlAsString.replace(/(\n|\r)-*/g, ''); // remove '\r\n-'. The dash is optional.
+                var xmldoc = Wicket.Xml.parse(xmlAsString);
+                return xmldoc;
+            };
+
+            /**
+             * Converts a NodeList to an Array
+             *
+             * @param nodeList The NodeList to convert
+             * @returns {Array} The array with document nodes
+             */
+            nodeListToArray = function (nodeList) {
+                var arr = [],
+                    nodeId;
+                if (nodeList && nodeList.length) {
+                    for (nodeId = 0; nodeId < nodeList.length; nodeId++) {
+                        arr.push(nodeList.item(nodeId));
+                    }
+                }
+                return arr;
+            };
+
+            /**
+             * Functions executer takes array of functions and executes them.
+             * The functions are executed one by one as far as the return value is FunctionsExecuter.DONE.
+             * If the return value is FunctionsExecuter.ASYNC or undefined then the execution of
+             * the functions will be resumed once the `notify` callback function is called.
+             * This is needed because header contributions need to do asynchronous download of JS and/or CSS
+             * and they have to let next function to run only after the download.
+             * After the FunctionsExecuter is initialized, the start methods triggers the first function.
+             *
+             * @param functions {Array} - an array of functions to execute
+             */
+            var FunctionsExecuter = function (functions) {
+
+                this.functions = functions;
+
+                /**
+                 * The index of the currently executed function
+                 * @type {number}
+                 */
+                this.current = 0;
+
+                /**
+                 * Tracks the depth of the call stack when `notify` is used for
+                 * asynchronous notification that a function execution has finished.
+                 * Should be reset to 0 when at some point to avoid problems like
+                 * "too much recursion". The reset may break the atomicity by allowing
+                 * another instance of FunctionExecuter to run its functions
+                 * @type {number}
+                 */
+                this.depth = 0; // we need to limit call stack depth
+
+                this.processNext = function () {
+                    if (this.current < this.functions.length) {
+                        var f, run;
+
+                        f = this.functions[this.current];
+                        run = function () {
+                            try {
+                                var n = jQuery.proxy(this.notify, this);
+                                return f(n);
+                            }
+                            catch (e) {
+                                Wicket.Log.error("FunctionsExecuter.processNext: " + e);
+                                return FunctionsExecuter.FAIL;
+                            }
+                        };
+                        run = jQuery.proxy(run, this);
+                        this.current++;
+
+                        if (this.depth > FunctionsExecuter.DEPTH_LIMIT) {
+                            // to prevent stack overflow (see WICKET-4675)
+                            this.depth = 0;
+                            window.setTimeout(run, 1);
+                        } else {
+                            var retValue = run();
+                            if (isUndef(retValue) || retValue === FunctionsExecuter.ASYNC) {
+                                this.depth++;
+                            }
+                            return retValue;
+                        }
+                    }
+                };
+
+                this.start = function () {
+                    var retValue = FunctionsExecuter.DONE;
+                    while (retValue === FunctionsExecuter.DONE) {
+                        retValue = this.processNext();
+                    }
+                };
+
+                this.notify = function () {
+                    this.start();
+                };
+            };
+
+            /**
+             * Response that should be used by a function when it finishes successfully
+             * in synchronous manner
+             * @type {number}
+             */
+            FunctionsExecuter.DONE = 1;
+
+            /**
+             * Response that should be used by a function when it finishes abnormally
+             * in synchronous manner
+             * @type {number}
+             */
+            FunctionsExecuter.FAIL = 2;
+
+            /**
+             * Response that may be used by a function when it executes asynchronous
+             * code and must wait `notify()` to be executed.
+             * @type {number}
+             */
+            FunctionsExecuter.ASYNC = 3;
+
+            /**
+             * An artificial number used as a limit of the call stack depth to avoid
+             * problems like "too much recursion" in the browser.
+             * The depth is not easy to be calculated because the memory used by the
+             * stack depends on many factors
+             * @type {number}
+             */
+            FunctionsExecuter.DEPTH_LIMIT = 1000;
+        }
+
     };
 })();
 
